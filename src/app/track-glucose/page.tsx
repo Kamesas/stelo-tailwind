bd459f4a38d577dfa3781777d51830364551f4e7
AlexSemak <semakaleksandr2014@gmail.com>
2026-03-16 14:00:59 +0200

    feat: add tracking glucose images

--- src/app/track-glucose/page.tsx
+++ src/app/track-glucose/page.tsx
@@ -2,7 +2,7 @@ import { TrackingGlucose } from "@/components/TrackingGlucose/TrackingGlucose";
 
 export default function WhyTrackGlucosePage() {
   return (
-    <div>
+    <div className="bg-white-200">
       <TrackingGlucose />
     </div>
   );
